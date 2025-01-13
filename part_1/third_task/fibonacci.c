e4800a080d1269b623b9437e139972f5446d4065
Jim <jimmanolis14@gmail.com>
2025-01-13 21:23:01 +0200

    fibonacci.c

--- part_1/third_task/fibonacci.c
+++ part_1/third_task/fibonacci.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-void calculateFibonacci(int n){
-	if (n < 1){
-		
+
+int calculateFibonacci(int n) {
+    if (n == 0) return 0;
+    if (n == 1) return 1;
+
+    int fib0 = 0;
+    int fib1 = 1;
+    int fibN = 0;
+
+    for (int i = 2; i <= n; i++) {
+        fibN = fib0 + fib1;
+        fib0 = fib1;
+        fib1 = fibN;
+    }
+
+    return fibN;
+}
+
+int main() {
+    int n = 15;
+
+    int result = calculateFibonacci(n);
+    if (result != -1) {
+        printf("fibonacci(%d) = %d\n", n, result);
+    }
+
+    return 0;
+}		
